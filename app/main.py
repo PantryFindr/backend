58870722c9a4ad7196871f17e505c958b7e7d585
mehul <github@mehapps.com>
2024-08-28 17:55:04 -0400

    basic zip code stuff?
    hopefully this works

--- app/main.py
+++ app/main.py
@@ -1,19 +1,38 @@
 from requests import get
 from html import unescape
-from json import dumps
+from json import dumps, load, dump
 from fastapi import FastAPI
+from os import environ, path
+from time import sleep
 
 pantries = {}
 
 NAME_END = -5
+ENV_PATH = ".env"
+DATABASE_PATH = "./app/pantries.json"
+SITE_URL = "https://mapping.littlefreepantry.org/"
+GEOCODE_KEY = environ.get("GEOCODE_KEY")
+
 
 app = FastAPI()
 
+if not path.exists(DATABASE_PATH):
+    with open(DATABASE_PATH, "w") as file:
+        file.write('{}')
+        
+with open(DATABASE_PATH) as file:
+    pantries = load(file)
+file.close()
+
+def save(pantries):
+    with open(DATABASE_PATH, "w") as file:
+        dump(pantries, file, indent=4)
+    file.close()
+
 @app.get("/")
 def get_locations():
-    if pantries != {}:
-        return pantries
-    response = (get("https://mapping.littlefreepantry.org/").text.splitlines())
+    response = get(SITE_URL).text.splitlines()
+    
     for line in response:
         if "push" in line:
             line = line.strip()
@@ -29,13 +48,20 @@ def get_locations():
             latitude = float(location[2].replace(",", ""))
             longitude = float(location[4].replace(",", ""))
             id = location[6].replace(",", "")
-            pantries[id] = {}
-            pantries[id]["name"] = name
-            pantries[id]["latitude"] = latitude
-            pantries[id]["longitude"] = longitude
-            print(f"Name: {name}, ID: {id}, Latitude: {latitude}, Longitude: {longitude}")
+            pantries[id] = pantries.get(id, {})
+            if pantries[id] == {}:
+                pantries[id]["name"] = name
+                pantries[id]["latitude"] = latitude
+                pantries[id]["longitude"] = longitude
+                print(f"https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}&api_key={GEOCODE_KEY}")
+                zipcode = get(f"https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}&api_key={GEOCODE_KEY}").text
+                print(zipcode)
+                # pantries[id]["zipcode"] = zipcode
+                sleep(1)
+            # print(f"Name: {name}, ID: {id}, Latitude: {latitude}, Longitude: {longitude}")
 
     print(dumps(pantries, indent=4))
+    save(pantries)
     return pantries
 
 @app.get("/faqs")
